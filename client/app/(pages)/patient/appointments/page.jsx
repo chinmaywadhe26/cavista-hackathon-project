51dd6c58c570b4c7c63dfac3ec2f1df65258837b
Egon <125190593+Egon15@users.noreply.github.com>
2025-02-22 16:36:50 +0530

    Stage 1

--- client/app/(pages)/patient/appointments/page.jsx
+++ client/app/(pages)/patient/appointments/page.jsx
@@ -1,5 +1,6 @@
 import AppointmentCard from "@/components/appointmentCard";
 import { hospital } from "lucide-react";
+import PatientForm from "@/components/patientForm";
 import React from "react";
 
 const appointmentsArray = [
@@ -61,10 +62,10 @@ const appointmentsArray = [
 
 const Appointments = () => {
   return (
-    <div className="w-screen h-dvh px-8 flex flex-col space-y-12 overflow-x-hidden">
+    <div className="w-screen h-dvh px-8 flex flex-col overflow-x-hidden">
       <h1 className="text-3xl font-bold">Appointments</h1>
-      <div className="flex w-full h-screen space-x-8">
-        <div className="w-1/2 flex flex-col h-3/4 bg-card p-4 text-card-foreground rounded-md border border-slate-300 shadow-md">
+      <div className="flex w-full h-3/4 mt-8 space-x-8">
+        <div className="w-1/2 flex flex-col h-full bg-card p-4 text-card-foreground rounded-md border border-slate-300 shadow-md">
           <h1 className="font-semibold">Today Appointment</h1>
           <div className="w-full h-full mt-4 space-y-1 bg-slate-200 rounded-md p-8">
             <h1 className="text-xl font-semibold">Hospital: Forte Hospital</h1>
@@ -72,7 +73,7 @@ const Appointments = () => {
             <h2 className="text-xl font-semibold">Doctor: Dr. K R Reddy</h2>
           </div>
         </div>
-        <div className="w-1/2 flex flex-col h-3/4 bg-card p-4 text-card-foreground rounded-md border border-slate-300 shadow-md">
+        <div className="w-1/2 flex flex-col h-full bg-card p-4 text-card-foreground rounded-md border border-slate-300 shadow-md">
           <h1 className="font-semibold">Upcoming appointments</h1>
           <div className="w-full h-full mt-4 overflow-y-scroll space-y-1 bg-slate-200 rounded-md">
             {appointmentsArray.map((appointment) => (
@@ -81,22 +82,17 @@ const Appointments = () => {
           </div>
         </div>
       </div>
-      <div className="w-full flex flex-col h-3/4 bg-card p-4 text-card-foreground rounded-md border border-slate-300 shadow-md">
+      <div className="w-full flex flex-col mt-8 h-3/4 bg-card p-4 text-card-foreground rounded-md border border-slate-300 shadow-md">
         <h1 className="font-semibold">Past appointments</h1>
-        <div className="w-full max-h-full mt-4 overflow-y-scroll space-y-1 bg-slate-200 rounded-md">
+        <div className="w-full h-full mt-4 overflow-y-scroll space-y-1 bg-slate-200 rounded-md">
           {appointmentsArray.map((appointment) => (
-            <div
-              className="flex space-x-8 h-44 border-b border-slate-300"
-              key={appointment.index}>
-              <h2>{appointment.date}</h2>
-              <h2>{appointment.time}</h2>
-              <h2>{appointment.doctor}</h2>
-            </div>
+            <AppointmentCard key={appointment.index} {...appointment} />
           ))}
         </div>
       </div>
-
-      <div></div>
+      <div>
+        <PatientForm />
+      </div>
     </div>
   );
 };
---
+++ client/components/patientForm.jsx
@@ -0,0 +1,389 @@
+"use client";
+import { useState } from "react";
+import { toast } from "sonner";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Slider } from "@/components/ui/slider";
+
+const formSchema = z.object({
+  pain_levels: z.number().min(0).max(10),
+  mobility_issues: z.number().min(0).max(10),
+  fatigue: z.number().min(0).max(10),
+  appetite_weight: z.number().min(0).max(10),
+  memory_issues: z.number().min(0).max(10),
+  disorientation: z.number().min(0).max(10),
+  mood_swings: z.number().min(0).max(10),
+  sleep_patterns: z.number().min(0).max(10),
+  breathing: z.number().min(0).max(10),
+  tremors_shaking: z.number().min(0).max(10),
+  numbness_tingling: z.number().min(0).max(10),
+  urination: z.number().min(0).max(10),
+  consultation: z.number().min(0).max(10),
+});
+
+export default function MyForm() {
+  //   const form = useForm < z.infer < typeof formSchema >> ({
+  //     resolver: zodResolver(formSchema),
+
+  //   })
+
+  //   function onSubmit() {
+  //     try {
+  //       console.log(values);
+  //       toast(
+  //         <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+  //           <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+  //         </pre>
+  //       );
+  //     } catch (error) {
+  //       console.error("Form submission error", error);
+  //       toast.error("Failed to submit the form. Please try again.");
+  //     }
+  //   }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 max-w-3xl mx-auto py-10">
+        <FormField
+          control={form.control}
+          name="pain_levels"
+          render={({ field: { value, onChange } }) => (
+            <FormItem>
+              <FormLabel>Price - {value}</FormLabel>
+              <FormControl>
+                <Slider
+                  min={0}
+                  max={10}
+                  step={1}
+                  defaultValue={[5]}
+                  onValueChange={(vals) => {
+                    onChange(vals[0]);
+                  }}
+                />
+              </FormControl>
+              <FormDescription>
+                Adjust the intensity by sliding.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="mobility_issues"
+          render={({ field: { value, onChange } }) => (
+            <FormItem>
+              <FormLabel>Price - {value}</FormLabel>
+              <FormControl>
+                <Slider
+                  min={0}
+                  max={10}
+                  step={1}
+                  defaultValue={[5]}
+                  onValueChange={(vals) => {
+                    onChange(vals[0]);
+                  }}
+                />
+              </FormControl>
+              <FormDescription>
+                Adjust the intensity by sliding.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="fatigue"
+          render={({ field: { value, onChange } }) => (
+            <FormItem>
+              <FormLabel>Price - {value}</FormLabel>
+              <FormControl>
+                <Slider
+                  min={0}
+                  max={10}
+                  step={1}
+                  defaultValue={[5]}
+                  onValueChange={(vals) => {
+                    onChange(vals[0]);
+                  }}
+                />
+              </FormControl>
+              <FormDescription>
+                Adjust the intensity by sliding.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="appetite_weight"
+          render={({ field: { value, onChange } }) => (
+            <FormItem>
+              <FormLabel>Price - {value}</FormLabel>
+              <FormControl>
+                <Slider
+                  min={0}
+                  max={10}
+                  step={1}
+                  defaultValue={[5]}
+                  onValueChange={(vals) => {
+                    onChange(vals[0]);
+                  }}
+                />
+              </FormControl>
+              <FormDescription>
+                Adjust the intensity by sliding.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="memory_issues"
+          render={({ field: { value, onChange } }) => (
+            <FormItem>
+              <FormLabel>Price - {value}</FormLabel>
+              <FormControl>
+                <Slider
+                  min={0}
+                  max={10}
+                  step={1}
+                  defaultValue={[5]}
+                  onValueChange={(vals) => {
+                    onChange(vals[0]);
+                  }}
+                />
+              </FormControl>
+              <FormDescription>
+                Adjust the intensity by sliding.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="disorientation"
+          render={({ field: { value, onChange } }) => (
+            <FormItem>
+              <FormLabel>Price - {value}</FormLabel>
+              <FormControl>
+                <Slider
+                  min={0}
+                  max={10}
+                  step={1}
+                  defaultValue={[5]}
+                  onValueChange={(vals) => {
+                    onChange(vals[0]);
+                  }}
+                />
+              </FormControl>
+              <FormDescription>
+                Adjust the intensity by sliding.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="mood_swings"
+          render={({ field: { value, onChange } }) => (
+            <FormItem>
+              <FormLabel>Price - {value}</FormLabel>
+              <FormControl>
+                <Slider
+                  min={0}
+                  max={10}
+                  step={1}
+                  defaultValue={[5]}
+                  onValueChange={(vals) => {
+                    onChange(vals[0]);
+                  }}
+                />
+              </FormControl>
+              <FormDescription>
+                Adjust the intensity by sliding.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="sleep_patterns"
+          render={({ field: { value, onChange } }) => (
+            <FormItem>
+              <FormLabel>Price - {value}</FormLabel>
+              <FormControl>
+                <Slider
+                  min={0}
+                  max={10}
+                  step={1}
+                  defaultValue={[5]}
+                  onValueChange={(vals) => {
+                    onChange(vals[0]);
+                  }}
+                />
+              </FormControl>
+              <FormDescription>
+                Adjust the intensity by sliding.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="breathing"
+          render={({ field: { value, onChange } }) => (
+            <FormItem>
+              <FormLabel>Price - {value}</FormLabel>
+              <FormControl>
+                <Slider
+                  min={0}
+                  max={10}
+                  step={1}
+                  defaultValue={[5]}
+                  onValueChange={(vals) => {
+                    onChange(vals[0]);
+                  }}
+                />
+              </FormControl>
+              <FormDescription>
+                Adjust the intensity by sliding.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="tremors_shaking"
+          render={({ field: { value, onChange } }) => (
+            <FormItem>
+              <FormLabel>Price - {value}</FormLabel>
+              <FormControl>
+                <Slider
+                  min={0}
+                  max={10}
+                  step={1}
+                  defaultValue={[5]}
+                  onValueChange={(vals) => {
+                    onChange(vals[0]);
+                  }}
+                />
+              </FormControl>
+              <FormDescription>
+                Adjust the intensity by sliding.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="numbness_tingling"
+          render={({ field: { value, onChange } }) => (
+            <FormItem>
+              <FormLabel>Price - {value}</FormLabel>
+              <FormControl>
+                <Slider
+                  min={0}
+                  max={10}
+                  step={1}
+                  defaultValue={[5]}
+                  onValueChange={(vals) => {
+                    onChange(vals[0]);
+                  }}
+                />
+              </FormControl>
+              <FormDescription>
+                Adjust the intensity by sliding.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="urination"
+          render={({ field: { value, onChange } }) => (
+            <FormItem>
+              <FormLabel>Price - {value}</FormLabel>
+              <FormControl>
+                <Slider
+                  min={0}
+                  max={10}
+                  step={1}
+                  defaultValue={[5]}
+                  onValueChange={(vals) => {
+                    onChange(vals[0]);
+                  }}
+                />
+              </FormControl>
+              <FormDescription>
+                Adjust the intensity by sliding.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="consultation"
+          render={({ field: { value, onChange } }) => (
+            <FormItem>
+              <FormLabel>Price - {value}</FormLabel>
+              <FormControl>
+                <Slider
+                  min={0}
+                  max={10}
+                  step={1}
+                  defaultValue={[5]}
+                  onValueChange={(vals) => {
+                    onChange(vals[0]);
+                  }}
+                />
+              </FormControl>
+              <FormDescription>
+                Adjust the intensity by sliding.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
