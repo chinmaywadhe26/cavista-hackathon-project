f4dd4ad0bfe187a4c2bc92f62ff5b835bcd73963
Egon <125190593+Egon15@users.noreply.github.com>
2025-02-22 16:41:29 +0530

    Update patientForm.jsx

--- client/components/patientForm.jsx
+++ client/components/patientForm.jsx
@@ -34,24 +34,23 @@ const formSchema = z.object({
 });
 
 export default function MyForm() {
-  //   const form = useForm < z.infer < typeof formSchema >> ({
-  //     resolver: zodResolver(formSchema),
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+  });
 
-  //   })
-
-  //   function onSubmit() {
-  //     try {
-  //       console.log(values);
-  //       toast(
-  //         <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-  //           <code className="text-white">{JSON.stringify(values, null, 2)}</code>
-  //         </pre>
-  //       );
-  //     } catch (error) {
-  //       console.error("Form submission error", error);
-  //       toast.error("Failed to submit the form. Please try again.");
-  //     }
-  //   }
+  function onSubmit(values) {
+    try {
+      console.log(values);
+      toast(
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      );
+    } catch (error) {
+      console.error("Form submission error", error);
+      toast.error("Failed to submit the form. Please try again.");
+    }
+  }
 
   return (
     <Form {...form}>
